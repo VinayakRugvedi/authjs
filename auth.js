b2c1823bda7fdb585ae9652a9535868ec310897f
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-12-28 11:11:43 +0530

    Add auth file which will be required by user with the desired functionalities

---
+++ auth.js
@@ -0,0 +1,17 @@
+const performValidations = require('./controllers/validateEmailAndPassword')
+const signUpVerification = require('./controllers/signUpVerification')
+const resendVerificationLink = require('./controllers/resendVerificationLink')
+const updateVerifiedStatus = require('./controllers/updateVerifiedStatus')
+const authenticateUser = require('./controllers/authenticateUser')
+const changePassword = require('./controllers/changePassword')
+
+const auth = {
+  performValidations, //Arguments : email, password, confirmPassword
+  signUpVerification, //Arguments : email and password
+  resendVerificationLink, //Arguments : email
+  updateVerifiedStatus, //Arguments : token
+  authenticateUser, //Arguments : email and password
+  changePassword, //Arguments : email and password
+}
+
+module.exports = auth
