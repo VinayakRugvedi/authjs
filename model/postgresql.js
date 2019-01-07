c32aa9621b3d24b37592577a216fc13cf28b2612
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-01-07 10:11:07 +0530

    Add otp related queries, modify insert query

--- model/postgresql.js
+++ model/postgresql.js
@@ -1,29 +1,37 @@
 const config = require('../config')
 
-const {Client} = require('pg')
+const { Client } = require('pg')
 const connectionString = config.dataBaseConfiguration.connectionString
 const client = new Client(connectionString)
 client.connect()
 
-//Formulating different queries
-function getInsertQuery(data) {
-  const insertQuery = {
-    name : 'store-user-info',
-    text : 'INSERT INTO data(token, email, expires, password, verified) VALUES($1, $2, $3, $4, $5) RETURNING *',
-    values : [`{${data.token}}`, `{${data.email}}`, data.expires, `{${data.password}}`, data.verified]
+// Formulating different queries
+function getInsertQuery (data, flag) {
+  let insertQuery
+  if (!flag) {
+    insertQuery = {
+      text : 'INSERT INTO data(token, email, expires, password, verified) VALUES($1, $2, $3, $4, $5) RETURNING *',
+      values : [data.token, data.email, data.expires, data.password, data.verified]
+    }
+  } else {
+    insertQuery = {
+      text : 'INSERT INTO data(token, otp, phoneNumber, expires, password, verified) VALUES($1, $2, $3, $4, $5, $6) RETURNING *',
+      values : [data.token, data.otp, data.phoneNumber, data.expires, data.password, data.verified]
+    }
   }
+
   return insertQuery
 }
 
-function getFetchQuery(field, value) {
+function getFetchQuery (field, value) {
   const fetchQuery = {
-    text : `SELECT * FROM data where ${field}[1] = $1`,
+    text : `SELECT * FROM data where ${field} = $1`,
     values : [value]
   }
   return fetchQuery
 }
 
-function getUpdateVerifiedQuery(id) {
+function getUpdateVerifiedQuery (id) {
   const updateQuery = {
     name : 'set-verified-to-true',
     text : 'UPDATE data SET verified = true WHERE _id = $1',
@@ -32,25 +40,34 @@ function getUpdateVerifiedQuery(id) {
   return updateQuery
 }
 
-function getupdateTokenAndExpiresQuery(id, token, expires) {
+function getupdateTokenAndExpiresQuery (id, token, expires) {
   const updateQuery = {
     name : 'set-newtoken-newexpires',
-    text : 'UPDATE data SET token[1] = $1, expires = $2 WHERE _id = $3',
+    text : 'UPDATE data SET token = $1, expires = $2 WHERE _id = $3',
     values : [token, expires, id]
   }
   return updateQuery
 }
 
-function getUpdatePasswordQuery(password, id) {
+function getupdateOtpAndExpiresQuery (id, otp, expires) {
+  const updateQuery = {
+    name : 'set-newotp-newexpires',
+    text : 'UPDATE data SET otp = $1, expires = $2 WHERE _id = $3',
+    values : [otp, expires, id]
+  }
+  return updateQuery
+}
+
+function getUpdatePasswordQuery (password, id) {
   const updateQuery = {
     name : 'set-new-password',
-    text : 'UPDATE data SET password[1] = $1 WHERE _id = $2',
+    text : 'UPDATE data SET password = $1 WHERE _id = $2',
     values : [password, id]
   }
   return updateQuery
 }
 
-function getDeleteQuery(id) {
+function getDeleteQuery (id) {
   const deleteQuery = {
     name : 'delete-user-data',
     text : 'DELETE FROM data WHERE _id = $1',
@@ -59,8 +76,8 @@ function getDeleteQuery(id) {
   return deleteQuery
 }
 
-async function insert(data) {
-  const insertQuery = getInsertQuery(data)
+async function insert (data, flag) {
+  const insertQuery = getInsertQuery(data, flag)
   const insertResult =
     await client.query(insertQuery)
       .catch((error) => console.log(error))
@@ -68,29 +85,30 @@ async function insert(data) {
   return insertResult
 }
 
-async function fetch(field, value) {
+async function fetch (field, value) {
   const fetchQuery = getFetchQuery(field, value)
+  console.log(fetchQuery)
   const fetchResult =
     await client.query(fetchQuery)
       .catch((error) => console.log(error))
   console.log(fetchResult, 'Fetch Result')
-  //Empty or not will be tested
-  if(fetchResult === undefined) return undefined
+  // Empty or not will be tested
+  if (fetchResult === undefined) return undefined
   return fetchResult.rows.length
-         ? structuredResultObject(fetchResult.rows[0])
-         : {}
+    ? fetchResult.rows[0]
+    : {}
 }
 
-async function updateVerified(id) {
+async function updateVerified (id) {
   const updateQuery = getUpdateVerifiedQuery(id)
   const updateResult =
     await client.query(updateQuery)
       .catch((error) => console.log(error))
-  console.log(updateResult, 'Update Result') //update.rows yeilds empty array
+  console.log(updateResult, 'Update Result') // update.rows yeilds empty array
   return updateResult
 }
 
-async function updateTokenAndExpires(id, token, expires) {
+async function updateTokenAndExpires (id, token, expires) {
   const updateQuery = getupdateTokenAndExpiresQuery(id, token, expires)
   const updateResult =
     await client.query(updateQuery)
@@ -99,7 +117,16 @@ async function updateTokenAndExpires(id, token, expires) {
   return updateResult
 }
 
-async function updatePassword(password, id) {
+async function updateOtpAndExpires (id, otp, expires) {
+  const updateQuery = getupdateOtpAndExpiresQuery(id, otp, expires)
+  const updateResult =
+    await client.query(updateQuery)
+      .catch((error) => console.log(error))
+  console.log(updateResult, 'Update Result')
+  return updateResult
+}
+
+async function updatePassword (password, id) {
   const updateQuery = getUpdatePasswordQuery(password, id)
   const updateResult =
     await client.query(updateQuery)
@@ -108,7 +135,7 @@ async function updatePassword(password, id) {
   return updateResult
 }
 
-async function deleteData(id) {
+async function deleteData (id) {
   const deleteQuery = getDeleteQuery(id)
   const deleteResult =
     await client.query(deleteQuery)
@@ -117,24 +144,12 @@ async function deleteData(id) {
   return deleteResult
 }
 
-function structuredResultObject(object) {
-  let result = {
-    _id : object._id,
-    token : object.token[0],
-    expires : object.expires,
-    email : object.email[0],
-    password : object.password[0],
-    verified : object.verified
-  }
-  console.log(result, 'result object')
-  return result
-}
-
 const postgresql = {
   insert,
   fetch,
   updateVerified,
   updateTokenAndExpires,
+  updateOtpAndExpires,
   updatePassword,
   deleteData
 }
