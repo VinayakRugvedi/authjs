8d2c7caa23a0df7a2f23f5033f75046cebfab35a
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-01-07 10:21:09 +0530

    Implement the phone number aspect

--- controllers/authenticateUser.js
+++ controllers/authenticateUser.js
@@ -3,43 +3,39 @@ const bcrypt = require('bcrypt')
 const config = require('../config')
 const dataBase = require(`../model/${config.dataBaseConfiguration.dataBase}`)
 
-async function authenticateUser(email, password) {
-  let passwordHash = await getHashIfAvailableAndAuthenticate(email)
-  if(passwordHash === false) {
-    console.log('There is no account registered with this email address; Register soon!')
-    // msg : "Looks like you havent created an account yet; Create one soon"
-  }
-  else if(passwordHash)
+async function authenticateUser (userName, password, flag = false) {
+  let passwordHash = await getHashIfAvailableAndAuthenticate(userName, flag)
+  console.log(passwordHash)
+  if (passwordHash === false)
+    console.log('There is no account registered with this userName; Register soon!')
+  else if (passwordHash === true)
     console.log('Your account has not been verified yet')
-    // msg : "Your account has not been verified yet!; Check your inbox or spam folder..."
-  else if(passwordHash !== undefined){
+  else if (typeof passwordHash === 'string') {
     let compareResult = await compare(passwordHash, password)
-    if(compareResult === undefined)
+    if (compareResult === undefined)
       console.log('Oops!, something went wrong; Try again...')
-    else if(compareResult === false)
+    else if (compareResult === false)
       console.log('Your credentials seems to be incorrect!')
-    else
-      console.log('You have been successfully authenticated')
+    else console.log('You have been successfully authenticated')
   }
 }
 
-async function getHashIfAvailableAndAuthenticate(email) {
-  let fetchResult = await dataBase.fetch('email', email)
-  if(fetchResult === undefined)
-  console.log('Couldnt retrive data; Try again after some time...')
-    //msg : 'Couldnt retrive data; Try again after some time...'
+async function getHashIfAvailableAndAuthenticate (userName, flag) {
+  let fetchResult = await dataBase.fetch(flag ? 'phonenumber' : 'email', userName)
+  if (fetchResult === undefined)
+  console.log('DB Error : OOPS, something went wrong')
   else {
-    if(Object.keys(fetchResult).length === 0)
+    if (Object.keys(fetchResult).length === 0)
       return false
-    else if(!fetchResult.verified)
+    else if (!fetchResult.verified)
       return true
     else return fetchResult.password
   }
 }
 
-async function compare(hash, password) {
+async function compare (hash, password) {
   let compareResult =
-    await bcrypt.compare(req.body.password, hash)
+    await bcrypt.compare(password, hash)
       .catch((error) => console.log(error))
 
   return compareResult
