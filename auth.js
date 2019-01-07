208afeb7da262a190784ebd213c07eb4236e83dd
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-01-07 10:14:21 +0530

    Expose otp related functions

--- auth.js
+++ auth.js
@@ -1,17 +1,19 @@
 const performValidations = require('./controllers/validateEmailAndPassword')
 const signUpVerification = require('./controllers/signUpVerification')
 const resendVerificationLink = require('./controllers/resendVerificationLink')
+const resendOtp = require('./controllers/resendOtp')
 const updateVerifiedStatus = require('./controllers/updateVerifiedStatus')
 const authenticateUser = require('./controllers/authenticateUser')
 const changePassword = require('./controllers/changePassword')
 
 const auth = {
-  performValidations, //Arguments : email, password, confirmPassword
-  signUpVerification, //Arguments : email and password
-  resendVerificationLink, //Arguments : email
-  updateVerifiedStatus, //Arguments : token
-  authenticateUser, //Arguments : email and password
-  changePassword, //Arguments : email and new password
+  performValidations, // Arguments : userName, password, confirmPassword
+  signUpVerification, // Arguments : userName, password, [flag](true for phoneNumber)
+  resendVerificationLink, // Arguments : email
+  resendOtp, // Arguments : Phone Number
+  updateVerifiedStatus, // Arguments : token or (phoneNumber and otp)
+  authenticateUser, // Arguments : email and password
+  changePassword // Arguments : email and newPassword
 }
 
 module.exports = auth
