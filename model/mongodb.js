d78b733bc6a63293395811ff3c2245bbe4ed9b81
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-01-07 10:12:21 +0530

    Add otp related queries, modify insert query

--- model/mongodb.js
+++ model/mongodb.js
@@ -5,7 +5,7 @@ const connectionString = config.dataBaseConfiguration.connectionString
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
-  useFindAndModify: false //For a deprecated warning
+  useFindAndModify: false // For a deprecated warning
 })
 
 const db = mongoose.connection
@@ -14,6 +14,8 @@ const userDataSchema = new mongoose.Schema({
   token : String,
   expires : Number,
   email : String,
+  otp : Number,
+  phonenumber : String,
   password : String,
   verified : Boolean
 })
@@ -30,68 +32,91 @@ db.once('open', () => {
   console.log('Successfully connected to the database...')
 })
 
-async function insert(data) {
-  let info = new userData({
-    token : data.token,
-    expires : data.expires,
-    email : data.email,
-    password : data.password,
-    verified : data.verified
-  })
+function getInsertInfo (data, flag) {
+  if(!flag) {
+    return new userData({
+      token : data.token,
+      expires : data.expires,
+      email : data.email,
+      password : data.password,
+      verified : data.verified
+    })
+  } else {
+    return new userData({
+      token : data.token,
+      expires : data.expires,
+      otp : data.otp,
+      phonenumber : data.phoneNumber,
+      password : data.password,
+      verified : data.verified
+    })
+  }
+}
 
+async function insert (data, flag) {
+  let info = getInsertInfo(data, flag)
   let insertResult =
     await info.save()
-      .catch((insertError) => console.log(insertError))
+      .catch(insertError => console.log(insertError))
   console.log(insertResult, 'Insert Result')
-  //insertResult is a document object
+  // insertResult is a document object
   return insertResult
 }
 
-async function fetch(key, value) {
+async function fetch (key, value) {
   let fetchResult =
-    await userData.find({ [key] : value }) //Computed Property
-      .catch((fetchError => console.log(fetchError)))
+    await userData.find({ [key] : value }) // Computed Property
+      .catch(fetchError => console.log(fetchError))
   console.log(fetchResult, 'Fetch Result')
-  //fetchResult is an array of objects
-  if(fetchResult === undefined) return undefined
+  // fetchResult is an array of objects
+  if (fetchResult === undefined) return undefined
   return fetchResult.length
-         ? fetchResult[0]
-         : {}
+    ? fetchResult[0]
+    : {}
 }
 
 async function updateVerified(id) {
   let updateResult =
     await userData.findOneAndUpdate({ _id : id }, { $set : { verified : true }})
-      .catch((updateError) => console.log(updateError))
+      .catch(updateError => console.log(updateError))
   console.log(updateResult, 'Update Result')
-  //updateResult is a document object
+  // updateResult is a document object
   return updateResult
 }
 
 async function updateTokenAndExpires(id, token, expires) {
   let updateResult =
     await userData.findOneAndUpdate({ _id : id }, { $set : { token, expires }})
-      .catch((updateError) => console.log(updateError))
+      .catch(updateError => console.log(updateError))
   console.log(updateResult, 'Update Result')
-  //updateResult is a document object
+  // updateResult is a document object
   return updateResult
 }
 
-async function updatePassword(password, id) {
+async function updateOtpAndExpires (id, otp, expires) {
+  let updateResult =
+    await userData.findOneAndUpdate({ _id : id }, { $set: { otp, expires }})
+      .catch(updateError => console.log(updateError))
+  console.log(updateResult, 'Update Result')
+  // updateResult is a document object
+  return updateResult
+}
+
+async function updatePassword (password, id) {
   let updateResult =
     await userData.findOneAndUpdate({ _id : id }, { $set : { password }})
-      .catch((updateError) => console.log(updateError))
+      .catch(updateError => console.log(updateError))
   console.log(updateResult, 'Update Result')
-  //updateResult is a document object
+  // updateResult is a document object
   return updateResult
 }
 
-async function deleteData(id) {
+async function deleteData (id) {
   let deleteResult =
     await userData.findOneAndDelete({ _id : id })
-      .catch((deleteError) => console.log(deleteError))
+      .catch(deleteError => console.log(deleteError))
   console.log(deleteResult, 'Update Result')
-  //deleteResult is a document object
+  // deleteResult is a document object
   return deleteResult
 }
 
@@ -100,6 +125,7 @@ const mongodb = {
   fetch,
   updateVerified,
   updateTokenAndExpires,
+  updateOtpAndExpires,
   updatePassword,
   deleteData
 }
