be2a11101a2bea8a665762598622c5a8a0b88428
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-01-10 09:10:27 +0530

    Implement return object and threw error

--- controllers/sendVerificationLink/sendgrid.js
+++ controllers/sendVerificationLink/sendgrid.js
@@ -5,26 +5,24 @@ const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(apiKey)
 
-function sendVerificationLink(email, token) {
-  let mail = {
-    from : 'Verifier <me@sendgrid.sample.org>',
-    to : `${email}`,
-    subject : 'Verifying You!',
-    html : `<p>Click the below link to verify yourself : </p>
+async function sendVerificationLink (email, token) {
+  const mail = {
+    from: 'Verifier <me@sendgrid.sample.org>',
+    to: `${email}`,
+    subject: 'Verifying You!',
+    html: `<p>Click the below link to verify yourself : </p>
             <a href="http://localhost:5000/verify/${token}">
                http://localhost:5000/verify/${token}</a>`
   }
 
-  sgMail.send(mail)
-    .then(() => {
-      console.log('Your data is securely stored and the verification link has been successfully sent; Check your inbox or spam folder')
-        // msg : "Your data is securely stored and the verification link has been successfully sent; Check your inbox or spam folder"
-    })
-    .catch((error) => {
-      console.log(error)
-      console.log('Your data is securely stored but coudnt send the verification link!; Try again...')
-        // msg : "Your data is securely stored but coudn't send the verification link!; Try again..."
-    })
+  const responseData =
+    await sgMail.send(mail)
+      .catch(error => { throw error })
+
+  return {
+    sendgridResponse: responseData,
+    message: `The user data is securely stored and the verification link has been sent to : ${email}`
+  }
 }
 
 module.exports = sendVerificationLink
