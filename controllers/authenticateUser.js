c40e355618706170d250b400da0ea255e5c63651
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-01-10 09:25:09 +0530

    Implement return object, throw error, eliminate unwanted variables

--- controllers/authenticateUser.js
+++ controllers/authenticateUser.js
@@ -4,39 +4,50 @@ const config = require('../config')
 const dataBase = require(`../model/${config.dataBaseConfiguration.dataBase}`)
 
 async function authenticateUser (userName, password, flag = false) {
-  let passwordHash = await getHashIfAvailableAndAuthenticate(userName, flag)
-  console.log(passwordHash)
+  const passwordHash =
+    await getHashIfAvailableAndAuthenticate(userName, flag)
+      .catch(error => { throw error })
+
   if (passwordHash === false)
-    console.log('There is no account registered with this userName; Register soon!')
+    return {
+      message: `The user : ${userName} has not yet registered!`
+    }
   else if (passwordHash === true)
-    console.log('Your account has not been verified yet')
+    return {
+      message: `The user : ${userName} has registered but not verified!`
+    }
   else if (typeof passwordHash === 'string') {
-    let compareResult = await compare(passwordHash, password)
-    if (compareResult === undefined)
-      console.log('Oops!, something went wrong; Try again...')
-    else if (compareResult === false)
-      console.log('Your credentials seems to be incorrect!')
-    else console.log('You have been successfully authenticated')
+    const compareResult =
+      await compare(passwordHash, password)
+        .catch(error => { throw error })
+
+    if (compareResult === false)
+      return {
+        message: `The users credentials seems to be incorrect`
+      }
+    else
+      return {
+        message: `The user is successfully authenticated`
+      }
   }
 }
 
 async function getHashIfAvailableAndAuthenticate (userName, flag) {
-  let fetchResult = await dataBase.fetch(flag ? 'phonenumber' : 'email', userName)
-  if (fetchResult === undefined)
-  console.log('DB Error : OOPS, something went wrong')
-  else {
-    if (Object.keys(fetchResult).length === 0)
-      return false
-    else if (!fetchResult.verified)
-      return true
-    else return fetchResult.password
-  }
+  const fetchResult =
+    await dataBase.fetch(flag ? 'phonenumber' : 'email', userName)
+      .catch(error => { throw error })
+
+  if (Object.keys(fetchResult).length === 0)
+    return false
+  else if (!fetchResult.verified)
+    return true
+  else return fetchResult.password
 }
 
 async function compare (hash, password) {
-  let compareResult =
+  const compareResult =
     await bcrypt.compare(password, hash)
-      .catch((error) => console.log(error))
+      .catch(error => { throw error })
 
   return compareResult
 }
