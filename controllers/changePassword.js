cc888762f2d6fd9209a301def77d29afe999710c
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-01-10 09:22:22 +0530

    Implement return object, throw error, eliminate unwanted variables

--- controllers/changePassword.js
+++ controllers/changePassword.js
@@ -3,25 +3,26 @@ const config = require('../config')
 const dataBase = require(`../model/${config.dataBaseConfiguration.dataBase}`)
 
 async function changePassword (userName, newPassword, flag = false) {
-  // lets say the newPassword is being sent here after being validated with confirm newPassword
-  // should update it
-  let fetchResult = await dataBase.fetch(flag ? 'phoneNumber' : 'email', userName)
-  if (fetchResult === undefined)
-    console.log('DB Error : OOPS, something went wrong')
-  else if (Object.keys(fetchResult).length === 0)
-    console.log('There is no account registered with this User Name; Register soon!')
+  const fetchResult =
+    await dataBase.fetch(flag ? 'phoneNumber' : 'email', userName)
+      .catch(error => { throw error })
+
+  if (Object.keys(fetchResult).length === 0)
+    return {
+      message: `The user : ${userName} has not yet registered!`
+    }
   else {
-    // user info found, update its newPassword
-    // first hash the newPassword
+    // user info found, update its newPassword by hashing
     const hash =
       await bcrypt.hash(newPassword, 10)
-        .catch((error) => console.log(error))
-    if (hash === undefined)
-      console.log('Couldnt secure your newPassword; Try again...')
-    let updateResult = await dataBase.updatePassword(hash, fetchResult._id)
-    if (updateResult === undefined)
-      console.log('DB Error : OOPS, something went wrong')
-    else console.log('Successfully updated your newPassword')
+        .catch(error => { throw error })
+
+    await dataBase.updatePassword(hash, fetchResult._id)
+      .catch(error => { throw error })
+
+    return {
+      message: `The user's(${userName}) password has been successfully updated`
+    }
   }
 }
 
