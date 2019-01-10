4012139179726b999c297878c1e6c23617eb4422
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-01-10 09:12:27 +0530

    Remove logs, implement return object, throw error

--- controllers/signUpVerification.js
+++ controllers/signUpVerification.js
@@ -5,45 +5,61 @@ const sendVerificationLink = require(`./sendVerificationLink/${config.mailConfig
 const sendOtp = require(`./sendOtp/${config.smsConfiguration.sender}`)
 
 async function signUpVerification (userName, password, flag = false) {
-  let userStatus =
+  const userStatus =
     await isUserRegistered(userName, password, flag)
-  if (userStatus === false) {
-    let userData = await hashPasswordAndFormulateUserObject(userName, password, flag)
-    if (userData === undefined)
-      console.log('Couldnt secure/hash your password; Try again') // Say hashing went wrong
-    else await storeUserData(userData, flag) // No user registered
-  }
-  else if (userStatus)
-    console.log('You have already registered but not verified')
-    // The user can request to send a verification link again
+      .catch(error => { throw error })
+
+  if (userStatus.isRegistered === false) {
+    const userData =
+      await hashPasswordAndFormulateUserObject(userName, password, flag)
+        .catch(error => { throw error })
+    const returnObject =
+      await storeUserDataAndSendVerification(userData, flag)
+        .catch(error => { throw error })
+    return returnObject
+  } else if (userStatus.isRegistered === true)
+    return {
+      message: userStatus.message
+    }
 }
 
 async function isUserRegistered (userName, password, flag) {
-  let fetchResult = await dataBase.fetch(flag ? 'phonenumber' : 'email', userName)
-  if (fetchResult === undefined)
-    console.log('Oops!, something went wrong; Try again...')
+  const fetchResult =
+    await dataBase.fetch(flag ? 'phonenumber' : 'email', userName)
+      .catch(error => { throw error })
+  if (Object.keys(fetchResult).length === 0)
+    return {
+      isRegistered: false
+    }
   else {
-    if (Object.keys(fetchResult).length === 0)
-      return false
-    else if (!fetchResult.verified)
-      return true // User registered but not verified
+    if (!fetchResult.verified)
+      return { // User registered but not verified
+        isRegistered: true,
+        message: `The user : ${userName} has already registered but not verified!`
+      }
     else
-      console.log('You have already been registered and verified - Just Sign Up with the credentials')
+      return {
+        isRegistered: true,
+        message: `The user : ${userName} has been registered and verified - User can now Sign In with his/her credentials`
+      }
   }
 }
 
-async function storeUserData (userData, flag) {
-  let insertResult = await dataBase.insert(userData, flag)
-  if(insertResult === undefined)
-    console.log('Oops!, couldnt save your data in the DB; Try again...')
-  else {
-    if (!flag) // Upon storing, send a Verification link or a OTP
-      sendVerificationLink(userData.email, userData.token)
-    else {
-      let from = config.smsConfiguration.from
-      sendOtp(from, userData.phoneNumber, userData.otp)
-    }
+async function storeUserDataAndSendVerification (userData, flag) {
+  await dataBase.insert(userData, flag)
+    .catch(error => { throw error })
+
+  let returnObject
+  if (!flag) {
+    returnObject =
+      await sendVerificationLink(userData.email, userData.token)
+        .catch(error => { throw error })
+  } else {
+    returnObject =
+      await sendOtp(userData.phoneNumber, userData.otp)
+        .catch(error => { throw error })
   }
+  return returnObject
 }
 
 module.exports = signUpVerification
