1da283ba5ea588cb374e3842e130ca1d1eeb2249
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-01-14 01:40:42 +0530

    Throw exception for invalid format of arguments, modify names

--- controllers/authenticateUser.js
+++ controllers/authenticateUser.js
@@ -1,22 +1,42 @@
+const validator = require('validator')
 const bcrypt = require('bcrypt')
 
-const config = require('../config')
-const dataBase = require(`../model/${config.dataBaseConfiguration.dataBase}`)
+const authConfig = require('../../../authConfig')
+const dataBase = require(`../model/${authConfig.dataBaseConfiguration.dataBase}`)
 
-async function authenticateUser (userName, password, flag = false) {
+async function authenticateUser (userName, password) {
+  let isPhone = false
+  if (!validator.isEmail(userName)) {
+    if (validator.isMobilePhone(userName, 'any', { strictMode:true })) isPhone = true
+    else {
+      throw new Error(
+        `
+        *******************************************************************************
+        The email/phoneNumber passed to 'signIn' function of auth
+        is not in the right format!
+
+        If you are passing a phone number ensure that it doesn't have any special
+        characters(except '+') and is in this format only :
+                   [+][country code][subscriber number including area code],
+        with no special characters!
+        *******************************************************************************
+        `)
+    }
+  }
+
   const passwordHash =
-    await getHashIfAvailableAndAuthenticate(userName, flag)
+    await getHashIfAvailableAndAuthenticate(userName, isPhone)
       .catch(error => { throw error })
 
   if (passwordHash === false)
     return {
       authCode: 13,
-      authMessage: `The user : ${userName} has not yet registered!`
+      authMessage: `The user : ${userName} has not yet registered! -- Cannot SignIn`
     }
   else if (passwordHash === true)
     return {
       authCode: 14,
-      authMessage: `The user : ${userName} has registered but not verified!`
+      authMessage: `The user : ${userName} has already registered but not verified! -- Cannot SignIn`
     }
   else if (typeof passwordHash === 'string') {
     const compareResult =
@@ -26,19 +46,19 @@ async function authenticateUser (userName, password, flag = false) {
     if (compareResult === false)
       return {
         authCode: 25,
-        authMessage: `The users credentials seems to be incorrect`
+        authMessage: `The user's(${userName}) credentials seems to be incorrect`
       }
     else
       return {
         authCode: 3,
-        authMessage: `The user is successfully authenticated`
+        authMessage: `The user : ${userName} is successfully authenticated/signedIn`
       }
   }
 }
 
-async function getHashIfAvailableAndAuthenticate (userName, flag) {
+async function getHashIfAvailableAndAuthenticate (userName, isPhone) {
   const fetchResult =
-    await dataBase.fetch(flag ? 'phonenumber' : 'email', userName)
+    await dataBase.fetch(isPhone ? 'phone' : 'email', userName)
       .catch(error => { throw error })
 
   if (Object.keys(fetchResult).length === 0)
