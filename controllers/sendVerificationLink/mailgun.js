d9e26fc4d48a8ac154e197a3350de672ade3b517
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-01-14 01:43:26 +0530

    Manage and throw exception for from, route and domain from authConfig, modify names

--- controllers/sendVerificationLink/mailgun.js
+++ controllers/sendVerificationLink/mailgun.js
@@ -1,6 +1,6 @@
-const config = require('../../config')
-const apiKey = config.mailConfiguration.apiKey
-const domain = config.mailConfiguration.domain
+const authConfig = require('../../../../authConfig')
+const apiKey = authConfig.mailConfiguration.apiKey
+const domain = authConfig.mailConfiguration.domain
 
 const mailgun = require('mailgun-js')({
   apiKey,
@@ -8,13 +8,54 @@ const mailgun = require('mailgun-js')({
 })
 
 async function sendVerificationLink (email, token) {
+  if(authConfig.mailConfiguration.domain === undefined ||
+     authConfig.mailConfiguration.domain.length === 0) {
+    throw new ReferenceError(
+      `
+        **********************************************************************************
+        Since you are using mailgun,
+        'domain' field is essential in the 'mailConfiguration' of the 'authConfig' file
+        For more detailed information regarding the domain and the authConfig file,
+        navigate to :
+        https://github.com/VinayakRugvedi/authjs/blob/master/authConfig.js
+        **********************************************************************************
+      `)
+  }
+
+  let from = 'Verifier <me@mailgun.sample.org>'
+  if (authConfig.mailConfiguration.from !== undefined &&
+     authConfig.mailConfiguration.from.length !== 0) {
+    from = authConfig.mailConfiguration.from
+  }
+
+  let route
+  if (authConfig.mailConfiguration.route === undefined ||
+     authConfig.mailConfiguration.route.length === 0) {
+    throw new ReferenceError(
+      `
+      **********************************************************************************
+      'route' field is essential in the 'mailConfiguration' of the 'authConfig' file
+      For more detailed information regarding the route and the authConfig file,
+      navigate to :
+      https://github.com/VinayakRugvedi/authjs/blob/master/authConfig.js
+      **********************************************************************************
+      `)
+  }
+  route = authConfig.mailConfiguration.route
+
+  let organizationName = 'TESTING'
+  if (authConfig.mailConfiguration.organizationName !== undefined &&
+      authConfig.mailConfiguration.organizationName.length !== 0) {
+    organizationName = authConfig.mailConfiguration.organizationName
+  }
+
   const mail = {
-    from: 'Verifier <me@mailgun.sample.org>',
+    from,
     to: `${email}`,
     subject: 'Verifying You!',
-    html: `<p>Click the below link to verify yourself : </p>
-            <a href="http://localhost:5000/verify/${token}">
-               http://localhost:5000/verify/${token}</a>`
+    html: `<p>Click the below link to get verified at ${organizationName} : </p>
+            <a href="${route}/${token}">
+            ${route}/${token}</a>`
   }
 
   function sendMail () {
