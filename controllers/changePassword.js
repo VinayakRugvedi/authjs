9b9fdb00c504455ec4a81c183a6690f8971ae168
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-01-14 01:40:20 +0530

    Throw exception for invalid format of arguments, modify names

--- controllers/changePassword.js
+++ controllers/changePassword.js
@@ -1,10 +1,31 @@
-const bcrypt = require('bcrypt')
-const config = require('../config')
-const dataBase = require(`../model/${config.dataBaseConfiguration.dataBase}`)
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
-async function changePassword (userName, newPassword, flag = false) {
+const authConfig = require('../../../authConfig')
+const dataBase = require(`../model/${authConfig.dataBaseConfiguration.dataBase}`)
+
+async function changePassword (userName, newPassword) {
+  let isPhone = false
+  if (!validator.isEmail(userName)) {
+    if (validator.isMobilePhone(userName, 'any', { strictMode:true })) isPhone = true
+    else {
+      throw new Error(
+        `
+          *******************************************************************************
+          The email/phoneNumber passed to 'changePassword' function of auth
+          is not in the right format!
+
+          If you are passing a phone number ensure that it doesn't have any special
+          characters(except '+') and is in this format only :
+                    [+][country code][subscriber number including area code],
+          with no special characters!
+          *******************************************************************************
+        `)
+    }
+  }
+
   const fetchResult =
-    await dataBase.fetch(flag ? 'phonenumber' : 'email', userName)
+    await dataBase.fetch(isPhone ? 'phone' : 'email', userName)
       .catch(error => { throw error })
 
   if (Object.keys(fetchResult).length === 0)
