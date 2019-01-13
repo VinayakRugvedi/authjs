964f0a65ae12ea2688b5a62372f7068746f577b2
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-01-14 01:35:44 +0530

    Throw exceptions for invalid format of arguments, modify names

--- controllers/signUpVerification.js
+++ controllers/signUpVerification.js
@@ -1,20 +1,41 @@
-const config = require('../config')
-const dataBase = require(`../model/${config.dataBaseConfiguration.dataBase}`)
-const hashPasswordAndFormulateUserObject = require('./hashPasswordAndFormulateUserObject')
-const sendVerificationLink = require(`./sendVerificationLink/${config.mailConfiguration.mailer}`)
-const sendOtp = require(`./sendOtp/${config.smsConfiguration.sender}`)
+const validator = require('validator')
 
-async function signUpVerification (userName, password, flag = false) {
+const authConfig = require('../../../authConfig')
+const dataBase = require(`../model/${authConfig.dataBaseConfiguration.dataBase}`)
+const hashPasswordAndFormulateUserObject = require('./hashPasswordAndFormulateUserObject')
+const sendVerificationLink = require(`./sendVerificationLink/${authConfig.mailConfiguration.mailer}`)
+const sendOtp = require(`./sendOtp/${authConfig.smsConfiguration.sender}`)
+
+async function signUpVerification (userName, password) {
+  let isPhone = false
+  if (!validator.isEmail(userName)) {
+    if (validator.isMobilePhone(userName, 'any', { strictMode: true })) isPhone = true
+    else {
+      throw new Error(
+        `
+          ********************************************************************************
+          The email/phoneNumber passed to 'signUp' function of auth
+          is not in the right format!
+
+          If you are passing a phone number ensure that it doesn't have any special
+          characters(except '+') and is in this format only :
+                    [+][country code][subscriber number including area code],
+          with no special characters!
+          ********************************************************************************
+        `)
+    }
+  }
+
   const userStatus =
-    await isUserRegisteredAndVerified(userName, password, flag)
+    await isUserRegisteredAndVerified(userName, password, isPhone)
       .catch(error => { throw error })
 
   if (userStatus.isRegistered === false) {
     const userData =
-      await hashPasswordAndFormulateUserObject(userName, password, flag)
+      await hashPasswordAndFormulateUserObject(userName, password, isPhone)
         .catch(error => { throw error })
     const returnObject =
-      await storeUserDataAndSendVerification(userData, flag)
+      await storeUserDataAndSendVerification(userData, isPhone)
         .catch(error => { throw error })
     return returnObject
   } else if (userStatus.isRegistered === true)
@@ -24,9 +45,9 @@ async function signUpVerification (userName, password, flag = false) {
     }
 }
 
-async function isUserRegisteredAndVerified (userName, password, flag) {
+async function isUserRegisteredAndVerified (userName, password, isPhone) {
   const fetchResult =
-    await dataBase.fetch(flag ? 'phonenumber' : 'email', userName)
+    await dataBase.fetch(isPhone ? 'phone' : 'email', userName)
       .catch(error => { throw error })
   if (Object.keys(fetchResult).length === 0)
     return {
@@ -48,18 +69,18 @@ async function isUserRegisteredAndVerified (userName, password, flag) {
   }
 }
 
-async function storeUserDataAndSendVerification (userData, flag) {
-  await dataBase.insert(userData, flag)
+async function storeUserDataAndSendVerification (userData, isPhone) {
+  await dataBase.insert(userData, isPhone)
     .catch(error => { throw error })
 
   let returnObject
-  if (!flag) {
+  if (!isPhone) {
     returnObject =
       await sendVerificationLink(userData.email, userData.token)
         .catch(error => { throw error })
   } else {
     returnObject =
-      await sendOtp(userData.phoneNumber, userData.otp)
+      await sendOtp(userData.phone, userData.otp)
         .catch(error => { throw error })
   }
   return returnObject
