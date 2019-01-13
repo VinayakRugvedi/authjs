2856216dfe01653ddf04fff91d729d0d2b70ecb3
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-01-14 01:44:20 +0530

    Manage and throw exception for from, route and organizationName from authConfig, modify names

--- controllers/sendVerificationLink/sendgrid.js
+++ controllers/sendVerificationLink/sendgrid.js
@@ -1,18 +1,44 @@
-const config = require('../../config')
-const apiKey = config.mailConfiguration.apiKey
+const authConfig = require('../../authConfig')
+const apiKey = authConfig.mailConfiguration.apiKey
 
 const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(apiKey)
 
 async function sendVerificationLink (email, token) {
+  let from = 'Verifier <me@sendgrid.sample.org>'
+  if (authConfig.mailConfiguration.from !== undefined &&
+     authConfig.mailConfiguration.from.length !== 0) {
+    from = authConfig.mailConfiguration.from
+  }
+
+  let route
+  if (authConfig.mailConfiguration.route === undefined ||
+     authConfig.mailConfiguration.route.length === 0) {
+    throw new ReferenceError(
+      `
+        **********************************************************************************
+        'route' field is essential in the 'mailConfiguration' of 'authConfig' file
+        For more detailed information regarding the route and the authConfig filenavigate to here :
+        https://github.com/VinayakRugvedi/authjs/blob/master/authConfig.js
+        **********************************************************************************
+      `)
+  }
+  route = authConfig.mailConfiguration.route
+
+  let organizationName = 'TESTING'
+  if (authConfig.mailConfiguration.organizationName !== undefined &&
+      authConfig.mailConfiguration.organizationName.length !== 0) {
+    organizationName = authConfig.mailConfiguration.organizationName
+  }
+
   const mail = {
-    from: 'Verifier <me@sendgrid.sample.org>',
+    from,
     to: `${email}`,
     subject: 'Verifying You!',
-    html: `<p>Click the below link to verify yourself : </p>
-            <a href="http://localhost:5000/verify/${token}">
-               http://localhost:5000/verify/${token}</a>`
+    html: `<p>Click the below link to get verified at ${organizationName} </p>
+            <a href="${route}/${token}">
+            ${route}/${token}</a>`
   }
 
   const responseData =
