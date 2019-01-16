e3513e758ce2bc5e8f4c12e20f2b9ba4dbe45d84
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-01-16 13:06:58 +0530

    Add executable file to automatically create authConfig file

---
+++ bin/createAuthConfig.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+
+fs.readFile('./node_modules/@vinayakrugvedi/authjs/authConfig.js',
+  (error, data) => {
+    if (error) {
+      console.log(error + '\n')
+      console.log('       OOPS, something went wrong, try again!')
+      console.log('       If this error still persists, kindly set up your authConfig file manually')
+      console.log('       Navigate here to know more about your authConfig file : ')
+      console.log('       https://github.com/VinayakRugvedi/authjs/blob/master/authConfig.js')
+      process.exit(1)
+    } else {
+      console.log(data.toString())
+      fs.writeFile('authConfig.js', data,
+        (error, data) => {
+          if (error) {
+            console.log(error + '\n')
+            console.log('       OOPS, something went wrong, try again!')
+            console.log('       If this error still persists, kindly set up your authConfig file manually')
+            console.log('       Navigate here to know more about your authConfig file : ')
+            console.log('       https://github.com/VinayakRugvedi/authjs/blob/master/authConfig.js')
+            process.exit(1)
+          } else {
+            console.log('Successfully created your authConfig.js file!!!')
+          }
+        })
+    }
+  })
