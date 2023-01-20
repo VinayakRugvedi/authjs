4e9b6b0a4b9e9ec5afdcf2130b1faa19a48b70b1
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-01-20 23:58:01 +0530

    comment console.logs + eslint fix

--- model/mongodb.js
+++ model/mongodb.js
@@ -1,20 +1,20 @@
-const authConfig = require('../../../../authConfig')
+const authConfig = require("../../../../authConfig");
 
-const mongoose = require('mongoose')
-const connectionString = authConfig.dataBaseConfiguration.connectionString
+const mongoose = require("mongoose");
+const connectionString = authConfig.dataBaseConfiguration.connectionString;
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
-  useFindAndModify: false // For a deprecated warning
-})
+  useFindAndModify: false, // For a deprecated warning
+});
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
 const UserDataSchema = new mongoose.Schema({
   token: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   expires: Number,
   email: String,
@@ -23,36 +23,36 @@ const UserDataSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   verified: {
     type: Boolean,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 
 // Compiling schema into model
-const AuthAccount = mongoose.model('authaccount', UserDataSchema)
+const AuthAccount = mongoose.model("authaccount", UserDataSchema);
 
-db.on('error', (error) => {
-  console.log('Error connectiong to the database!')
-  console.log(error)
-  process.exit(0)
-})
+db.on("error", (error) => {
+  console.log("Error connectiong to the database!");
+  console.log(error);
+  process.exit(0);
+});
 
-db.once('open', () => {
-  console.log('\nSuccessfully connected to the database...\n')
-})
+db.once("open", () => {
+  console.log("\nSuccessfully connected to the database...\n");
+});
 
-function getInsertInfo (data, isPhone) {
+function getInsertInfo(data, isPhone) {
   if (!isPhone) {
     return new AuthAccount({
       token: data.token,
       expires: data.expires,
       email: data.email,
       password: data.password,
-      verified: data.verified
-    })
+      verified: data.verified,
+    });
   } else {
     return new AuthAccount({
       token: data.token,
@@ -60,76 +60,89 @@ function getInsertInfo (data, isPhone) {
       otp: data.otp,
       phone: data.phone,
       password: data.password,
-      verified: data.verified
-    })
+      verified: data.verified,
+    });
   }
 }
 
-async function insert (data, isPhone) {
-  let info = getInsertInfo(data, isPhone)
-  let insertResult =
-    await info.save()
-      .catch(error => { throw error })
-  console.log(insertResult, 'Insert Result')
+async function insert(data, isPhone) {
+  let info = getInsertInfo(data, isPhone);
+  let insertResult = await info.save().catch((error) => {
+    throw error;
+  });
+  // console.log(insertResult, 'Insert Result')
   // insertResult is a document object
-  return insertResult
+  return insertResult;
 }
 
-async function fetch (key, value) {
-  let fetchResult =
-    await AuthAccount.find({ [key]: value }) // Computed Property
-      .catch(error => { throw error })
-  console.log(fetchResult, 'Fetch Result')
+async function fetch(key, value) {
+  let fetchResult = await AuthAccount.find({ [key]: value }) // Computed Property
+    .catch((error) => {
+      throw error;
+    });
+  // console.log(fetchResult, 'Fetch Result')
   // fetchResult is an array of objects
-  if (fetchResult === undefined) return undefined
-  return fetchResult.length
-    ? fetchResult[0]
-    : {}
+  if (fetchResult === undefined) return undefined;
+  return fetchResult.length ? fetchResult[0] : {};
 }
 
-async function updateVerified (id) {
-  let updateResult =
-    await AuthAccount.findOneAndUpdate({ _id: id }, { $set: { verified: true } })
-      .catch(error => { throw error })
-  console.log(updateResult, 'Update Result')
+async function updateVerified(id) {
+  let updateResult = await AuthAccount.findOneAndUpdate(
+    { _id: id },
+    { $set: { verified: true } }
+  ).catch((error) => {
+    throw error;
+  });
+  // console.log(updateResult, 'Update Result')
   // updateResult is a document object
-  return updateResult
+  return updateResult;
 }
 
-async function updateTokenAndExpires (id, token, expires) {
-  let updateResult =
-    await AuthAccount.findOneAndUpdate({ _id: id }, { $set: { token, expires } })
-      .catch(error => { throw error })
-  console.log(updateResult, 'Update Result')
+async function updateTokenAndExpires(id, token, expires) {
+  let updateResult = await AuthAccount.findOneAndUpdate(
+    { _id: id },
+    { $set: { token, expires } }
+  ).catch((error) => {
+    throw error;
+  });
+  // console.log(updateResult, 'Update Result')
   // updateResult is a document object
-  return updateResult
+  return updateResult;
 }
 
-async function updateOtpAndExpires (id, otp, expires) {
-  let updateResult =
-    await AuthAccount.findOneAndUpdate({ _id: id }, { $set: { otp, expires } })
-      .catch(error => { throw error })
-  console.log(updateResult, 'Update Result')
+async function updateOtpAndExpires(id, otp, expires) {
+  let updateResult = await AuthAccount.findOneAndUpdate(
+    { _id: id },
+    { $set: { otp, expires } }
+  ).catch((error) => {
+    throw error;
+  });
+  // console.log(updateResult, 'Update Result')
   // updateResult is a document object
-  return updateResult
+  return updateResult;
 }
 
-async function updatePassword (password, id) {
-  let updateResult =
-    await AuthAccount.findOneAndUpdate({ _id: id }, { $set: { password } })
-      .catch(error => { throw error })
-  console.log(updateResult, 'Update Result')
+async function updatePassword(password, id) {
+  let updateResult = await AuthAccount.findOneAndUpdate(
+    { _id: id },
+    { $set: { password } }
+  ).catch((error) => {
+    throw error;
+  });
+  // console.log(updateResult, 'Update Result')
   // updateResult is a document object
-  return updateResult
+  return updateResult;
 }
 
-async function deleteData (id) {
-  let deleteResult =
-    await AuthAccount.findOneAndDelete({ _id: id })
-      .catch(error => { throw error })
-  console.log(deleteResult, 'Update Result')
+async function deleteData(id) {
+  let deleteResult = await AuthAccount.findOneAndDelete({ _id: id }).catch(
+    (error) => {
+      throw error;
+    }
+  );
+  // console.log(deleteResult, 'Update Result')
   // deleteResult is a document object
-  return deleteResult
+  return deleteResult;
 }
 
 const mongodb = {
@@ -139,7 +152,7 @@ const mongodb = {
   updateTokenAndExpires,
   updateOtpAndExpires,
   updatePassword,
-  deleteData
-}
+  deleteData,
+};
 
-module.exports = mongodb
+module.exports = mongodb;
--- model/postgresql.js
+++ model/postgresql.js
@@ -1,21 +1,19 @@
-const authConfig = require('../../../../authConfig')
+const authConfig = require("../../../../authConfig");
 
-const { Client } = require('pg')
-const connectionString = authConfig.dataBaseConfiguration.connectionString
+const { Client } = require("pg");
+const connectionString = authConfig.dataBaseConfiguration.connectionString;
 const client = new Client(connectionString);
 
 (async function () {
-  await client.connect()
-    .catch(error => {
-      console.log(error)
-      process.exit(0)
-    })
-  console.log('\nSuccessfully connected to the database...\n')
+  await client.connect().catch((error) => {
+    console.log(error);
+    process.exit(0);
+  });
+  console.log("\nSuccessfully connected to the database...\n");
 })();
 
 (async function () {
-  const createTableQuery =
-  `CREATE TABLE IF NOT EXISTS authaccount (
+  const createTableQuery = `CREATE TABLE IF NOT EXISTS authaccount (
     _id serial PRIMARY KEY,
     expires BIGINT NOT NULL,
     verified BOOLEAN NOT NULL,
@@ -24,148 +22,158 @@ const client = new Client(connectionString);
     password CHARACTER (60) NOT NULL,
     phone VARCHAR (20) UNIQUE,
     otp integer
-  )`
+  )`;
 
-  await client.query(createTableQuery)
-    .catch(error => {
-      console.log(error)
-      process.exit(0)
-    })
-})()
+  await client.query(createTableQuery).catch((error) => {
+    console.log(error);
+    process.exit(0);
+  });
+})();
 
 // Formulating different queries
-function getInsertQuery (data, isPhone) {
-  let insertQuery
+function getInsertQuery(data, isPhone) {
+  let insertQuery;
   if (!isPhone) {
     insertQuery = {
-      text: 'INSERT INTO authaccount(token, email, expires, password, verified) VALUES($1, $2, $3, $4, $5) RETURNING *',
-      values: [data.token, data.email, data.expires, data.password, data.verified]
-    }
+      text: "INSERT INTO authaccount(token, email, expires, password, verified) VALUES($1, $2, $3, $4, $5) RETURNING *",
+      values: [
+        data.token,
+        data.email,
+        data.expires,
+        data.password,
+        data.verified,
+      ],
+    };
   } else {
     insertQuery = {
-      text: 'INSERT INTO authaccount(token, otp, phone, expires, password, verified) VALUES($1, $2, $3, $4, $5, $6) RETURNING *',
-      values: [data.token, data.otp, data.phone, data.expires, data.password, data.verified]
-    }
+      text: "INSERT INTO authaccount(token, otp, phone, expires, password, verified) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+      values: [
+        data.token,
+        data.otp,
+        data.phone,
+        data.expires,
+        data.password,
+        data.verified,
+      ],
+    };
   }
-  return insertQuery
+  return insertQuery;
 }
 
-function getFetchQuery (field, value) {
+function getFetchQuery(field, value) {
   const fetchQuery = {
     text: `SELECT * FROM authaccount where ${field} = $1`,
-    values: [value]
-  }
-  return fetchQuery
+    values: [value],
+  };
+  return fetchQuery;
 }
 
-function getUpdateVerifiedQuery (id) {
+function getUpdateVerifiedQuery(id) {
   const updateQuery = {
-    name: 'set-verified-to-true',
-    text: 'UPDATE authaccount SET verified = true WHERE _id = $1',
-    values: [id]
-  }
-  return updateQuery
+    name: "set-verified-to-true",
+    text: "UPDATE authaccount SET verified = true WHERE _id = $1",
+    values: [id],
+  };
+  return updateQuery;
 }
 
-function getupdateTokenAndExpiresQuery (id, token, expires) {
+function getupdateTokenAndExpiresQuery(id, token, expires) {
   const updateQuery = {
-    name: 'set-newtoken-newexpires',
-    text: 'UPDATE authaccount SET token = $1, expires = $2 WHERE _id = $3',
-    values: [token, expires, id]
-  }
-  return updateQuery
+    name: "set-newtoken-newexpires",
+    text: "UPDATE authaccount SET token = $1, expires = $2 WHERE _id = $3",
+    values: [token, expires, id],
+  };
+  return updateQuery;
 }
 
-function getupdateOtpAndExpiresQuery (id, otp, expires) {
+function getupdateOtpAndExpiresQuery(id, otp, expires) {
   const updateQuery = {
-    name: 'set-newotp-newexpires',
-    text: 'UPDATE authaccount SET otp = $1, expires = $2 WHERE _id = $3',
-    values: [otp, expires, id]
-  }
-  return updateQuery
+    name: "set-newotp-newexpires",
+    text: "UPDATE authaccount SET otp = $1, expires = $2 WHERE _id = $3",
+    values: [otp, expires, id],
+  };
+  return updateQuery;
 }
 
-function getUpdatePasswordQuery (password, id) {
+function getUpdatePasswordQuery(password, id) {
   const updateQuery = {
-    name: 'set-new-password',
-    text: 'UPDATE authaccount SET password = $1 WHERE _id = $2',
-    values: [password, id]
-  }
-  return updateQuery
+    name: "set-new-password",
+    text: "UPDATE authaccount SET password = $1 WHERE _id = $2",
+    values: [password, id],
+  };
+  return updateQuery;
 }
 
-function getDeleteQuery (id) {
+function getDeleteQuery(id) {
   const deleteQuery = {
-    name: 'delete-user-data',
-    text: 'DELETE FROM authaccount WHERE _id = $1',
-    values: [id]
-  }
-  return deleteQuery
+    name: "delete-user-data",
+    text: "DELETE FROM authaccount WHERE _id = $1",
+    values: [id],
+  };
+  return deleteQuery;
 }
 
-async function insert (data, isPhone) {
-  const insertQuery = getInsertQuery(data, isPhone)
-  const insertResult =
-    await client.query(insertQuery)
-      .catch(error => { throw error })
-  console.log(insertResult.rows, 'Insert Result')
-  return insertResult
+async function insert(data, isPhone) {
+  const insertQuery = getInsertQuery(data, isPhone);
+  const insertResult = await client.query(insertQuery).catch((error) => {
+    throw error;
+  });
+  // console.log(insertResult.rows, 'Insert Result')
+  return insertResult;
 }
 
-async function fetch (field, value) {
-  const fetchQuery = getFetchQuery(field, value)
-  const fetchResult =
-    await client.query(fetchQuery)
-      .catch(error => { throw error })
-  console.log(fetchResult.rows, 'Fetch Result')
-  return fetchResult.rows.length
-    ? fetchResult.rows[0]
-    : {}
+async function fetch(field, value) {
+  const fetchQuery = getFetchQuery(field, value);
+  const fetchResult = await client.query(fetchQuery).catch((error) => {
+    throw error;
+  });
+  // console.log(fetchResult.rows, 'Fetch Result')
+  return fetchResult.rows.length ? fetchResult.rows[0] : {};
 }
 
-async function updateVerified (id) {
-  const updateQuery = getUpdateVerifiedQuery(id)
-  const updateResult =
-    await client.query(updateQuery)
-      .catch(error => { throw error })
-  console.log(updateResult, 'Update Result') // update.rows yeilds empty array
-  return updateResult
+async function updateVerified(id) {
+  const updateQuery = getUpdateVerifiedQuery(id);
+  const updateResult = await client.query(updateQuery).catch((error) => {
+    throw error;
+  });
+  // console.log(updateResult, 'Update Result') // update.rows yeilds empty array
+  return updateResult;
 }
 
-async function updateTokenAndExpires (id, token, expires) {
-  const updateQuery = getupdateTokenAndExpiresQuery(id, token, expires)
-  const updateResult =
-    await client.query(updateQuery)
-      .catch(error => { throw error })
-  console.log(updateResult, 'Update Result')
-  return updateResult
+async function updateTokenAndExpires(id, token, expires) {
+  const updateQuery = getupdateTokenAndExpiresQuery(id, token, expires);
+  const updateResult = await client.query(updateQuery).catch((error) => {
+    throw error;
+  });
+  // console.log(updateResult, 'Update Result')
+  return updateResult;
 }
 
-async function updateOtpAndExpires (id, otp, expires) {
-  const updateQuery = getupdateOtpAndExpiresQuery(id, otp, expires)
-  const updateResult =
-    await client.query(updateQuery)
-      .catch(error => { throw error })
-  console.log(updateResult, 'Update Result')
-  return updateResult
+async function updateOtpAndExpires(id, otp, expires) {
+  const updateQuery = getupdateOtpAndExpiresQuery(id, otp, expires);
+  const updateResult = await client.query(updateQuery).catch((error) => {
+    throw error;
+  });
+  // console.log(updateResult, 'Update Result')
+  return updateResult;
 }
 
-async function updatePassword (password, id) {
-  const updateQuery = getUpdatePasswordQuery(password, id)
-  const updateResult =
-    await client.query(updateQuery)
-      .catch(error => { throw error })
-  console.log(updateResult, 'Update Result')
-  return updateResult
+async function updatePassword(password, id) {
+  const updateQuery = getUpdatePasswordQuery(password, id);
+  const updateResult = await client.query(updateQuery).catch((error) => {
+    throw error;
+  });
+  // console.log(updateResult, 'Update Result')
+  return updateResult;
 }
 
-async function deleteData (id) {
-  const deleteQuery = getDeleteQuery(id)
-  const deleteResult =
-    await client.query(deleteQuery)
-      .catch(error => { throw error })
-  console.log(deleteResult, 'Delete Result')
-  return deleteResult
+async function deleteData(id) {
+  const deleteQuery = getDeleteQuery(id);
+  const deleteResult = await client.query(deleteQuery).catch((error) => {
+    throw error;
+  });
+  // console.log(deleteResult, 'Delete Result')
+  return deleteResult;
 }
 
 const postgresql = {
@@ -175,7 +183,7 @@ const postgresql = {
   updateTokenAndExpires,
   updateOtpAndExpires,
   updatePassword,
-  deleteData
-}
+  deleteData,
+};
 
-module.exports = postgresql
+module.exports = postgresql;
