e08e27485ecc0bfba2c019df5c338bb24bf56d1b
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-01-21 00:00:03 +0530

    eslint fix

--- controllers/sendVerificationLink/mailgun.js
+++ controllers/sendVerificationLink/mailgun.js
@@ -1,15 +1,17 @@
-const authConfig = require('../../../../../authConfig')
-const apiKey = authConfig.mailConfiguration.apiKey
-const domain = authConfig.mailConfiguration.domain
+const authConfig = require("../../../../../authConfig");
+const apiKey = authConfig.mailConfiguration.apiKey;
+const domain = authConfig.mailConfiguration.domain;
 
-const mailgun = require('mailgun-js')({
+const mailgun = require("mailgun-js")({
   apiKey,
-  domain
-})
+  domain,
+});
 
-async function sendVerificationLink (email, token) {
-  if (authConfig.mailConfiguration.domain === undefined ||
-     authConfig.mailConfiguration.domain.length === 0) {
+async function sendVerificationLink(email, token) {
+  if (
+    authConfig.mailConfiguration.domain === undefined ||
+    authConfig.mailConfiguration.domain.length === 0
+  ) {
     throw new ReferenceError(
       `
         **********************************************************************************
@@ -20,18 +22,23 @@ async function sendVerificationLink (email, token) {
         navigate to :
         https://github.com/VinayakRugvedi/authjs/blob/master/authConfig.js
         **********************************************************************************
-      `)
+      `
+    );
   }
 
-  let from = 'Verifier <me@mailgun.sample.org>'
-  if (authConfig.mailConfiguration.from !== undefined &&
-     authConfig.mailConfiguration.from.length !== 0) {
-    from = authConfig.mailConfiguration.from
+  let from = "Verifier <me@mailgun.sample.org>";
+  if (
+    authConfig.mailConfiguration.from !== undefined &&
+    authConfig.mailConfiguration.from.length !== 0
+  ) {
+    from = authConfig.mailConfiguration.from;
   }
 
-  let route
-  if (authConfig.mailConfiguration.route === undefined ||
-     authConfig.mailConfiguration.route.length === 0) {
+  let route;
+  if (
+    authConfig.mailConfiguration.route === undefined ||
+    authConfig.mailConfiguration.route.length === 0
+  ) {
     throw new ReferenceError(
       `
       **********************************************************************************
@@ -41,43 +48,46 @@ async function sendVerificationLink (email, token) {
       navigate to :
       https://github.com/VinayakRugvedi/authjs/blob/master/authConfig.js
       **********************************************************************************
-      `)
+      `
+    );
   }
-  route = authConfig.mailConfiguration.route
+  route = authConfig.mailConfiguration.route;
 
-  let organizationName = 'TESTING'
-  if (authConfig.mailConfiguration.organizationName !== undefined &&
-      authConfig.mailConfiguration.organizationName.length !== 0) {
-    organizationName = authConfig.mailConfiguration.organizationName
+  let organizationName = "TESTING";
+  if (
+    authConfig.mailConfiguration.organizationName !== undefined &&
+    authConfig.mailConfiguration.organizationName.length !== 0
+  ) {
+    organizationName = authConfig.mailConfiguration.organizationName;
   }
 
   const mail = {
     from,
     to: `${email}`,
-    subject: 'Verifying You!',
+    subject: "Verifying You!",
     html: `<p>Click the below link to get verified at ${organizationName} : </p>
             <a href="${route}/${token}">
-            ${route}/${token}</a>`
-  }
+            ${route}/${token}</a>`,
+  };
 
-  function sendMail () {
+  function sendMail() {
     return new Promise((resolve, reject) => {
       mailgun.messages().send(mail, (error, body) => {
-        if (error) reject(error)
-        else resolve(body)
-      })
-    })
+        if (error) reject(error);
+        else resolve(body);
+      });
+    });
   }
 
-  const responseData =
-    await sendMail()
-      .catch(error => { throw error })
+  const responseData = await sendMail().catch((error) => {
+    throw error;
+  });
 
   return {
     mailgunResponse: responseData,
     authCode: 3,
-    authMessage: `The user data is securely stored and the verification link has been sent to : ${email}`
-  }
+    authMessage: `The user data is securely stored and the verification link has been sent to : ${email}`,
+  };
 }
 
-module.exports = sendVerificationLink
+module.exports = sendVerificationLink;
--- controllers/sendVerificationLink/sendgrid.js
+++ controllers/sendVerificationLink/sendgrid.js
@@ -1,20 +1,24 @@
-const authConfig = require('../../../../../authConfig')
-const apiKey = authConfig.mailConfiguration.apiKey
+const authConfig = require("../../../../../authConfig");
+const apiKey = authConfig.mailConfiguration.apiKey;
 
-const sgMail = require('@sendgrid/mail')
+const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(apiKey)
+sgMail.setApiKey(apiKey);
 
-async function sendVerificationLink (email, token) {
-  let from = 'Verifier <me@sendgrid.sample.org>'
-  if (authConfig.mailConfiguration.from !== undefined &&
-     authConfig.mailConfiguration.from.length !== 0) {
-    from = authConfig.mailConfiguration.from
+async function sendVerificationLink(email, token) {
+  let from = "Verifier <me@sendgrid.sample.org>";
+  if (
+    authConfig.mailConfiguration.from !== undefined &&
+    authConfig.mailConfiguration.from.length !== 0
+  ) {
+    from = authConfig.mailConfiguration.from;
   }
 
-  let route
-  if (authConfig.mailConfiguration.route === undefined ||
-     authConfig.mailConfiguration.route.length === 0) {
+  let route;
+  if (
+    authConfig.mailConfiguration.route === undefined ||
+    authConfig.mailConfiguration.route.length === 0
+  ) {
     throw new ReferenceError(
       `
         **********************************************************************************
@@ -24,34 +28,37 @@ async function sendVerificationLink (email, token) {
         navigate to here :
         https://github.com/VinayakRugvedi/authjs/blob/master/authConfig.js
         **********************************************************************************
-      `)
+      `
+    );
   }
-  route = authConfig.mailConfiguration.route
+  route = authConfig.mailConfiguration.route;
 
-  let organizationName = 'TESTING'
-  if (authConfig.mailConfiguration.organizationName !== undefined &&
-      authConfig.mailConfiguration.organizationName.length !== 0) {
-    organizationName = authConfig.mailConfiguration.organizationName
+  let organizationName = "TESTING";
+  if (
+    authConfig.mailConfiguration.organizationName !== undefined &&
+    authConfig.mailConfiguration.organizationName.length !== 0
+  ) {
+    organizationName = authConfig.mailConfiguration.organizationName;
   }
 
   const mail = {
     from,
     to: `${email}`,
-    subject: 'Verifying You!',
+    subject: "Verifying You!",
     html: `<p>Click the below link to get verified at ${organizationName} </p>
             <a href="${route}/${token}">
-            ${route}/${token}</a>`
-  }
+            ${route}/${token}</a>`,
+  };
 
-  const responseData =
-    await sgMail.send(mail)
-      .catch(error => { throw error })
+  const responseData = await sgMail.send(mail).catch((error) => {
+    throw error;
+  });
 
   return {
     sendgridResponse: responseData,
     authCode: 3,
-    authMessage: `The user data is securely stored and the verification link has been sent to : ${email}`
-  }
+    authMessage: `The user data is securely stored and the verification link has been sent to : ${email}`,
+  };
 }
 
-module.exports = sendVerificationLink
+module.exports = sendVerificationLink;
